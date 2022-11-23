9396d5a296687c59d2142baddaca70ac29309667
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-23 23:59:11 +0530

    Add files via upload

---
+++ strings1.py
@@ -0,0 +1,15 @@
+'''
+DAY:WEDNESDAY
+DATE:23rd NOV 2022
+TOPIC:STRING
+AUTHOR:POOJA
+'''
+a='kiran'
+d='*'.join(a)
+print(d) #k*i*r*a*n
+b='pooja'
+c='#'.join(b)
+print(c) # p#o#o#j#a
+e='prasanna'
+f='&'.join(e)
+print(f) # p&r&a&s&a&n&n&a
